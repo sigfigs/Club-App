67f4b4393e16e5b49595795e0bf8657ca3bfb3a2
Jeffrey Li <jeffreyli8000@gmail.com>
2023-01-24 08:54:44 -0500

    class DB

--- lib/clubs_db_3.dart
+++ lib/clubs_db_3.dart
@@ -1,11 +1,5 @@
 import 'package:postgres/postgres.dart';
 
-// List<List<dynamic>> ids = [];
-// List<List<dynamic>> clubNames = [];
-// List<List<dynamic>> categorys = [];
-// List<List<dynamic>> meetingDays = [];
-// List<List<dynamic>> advisorNames = [];
-// List<List<dynamic>> advisorEmails = [];
 List<List<dynamic>> monkey = [];
 List<List<dynamic>> userdata = [];
 Map<String, int> categorySizes = {
@@ -22,32 +16,52 @@ Map<String, int> categorySizes = {
   "Activities": 0
 };
 
-void main() async {
-  var connection = PostgreSQLConnection(
-    "localhost",
-    5432,
-    "clubs",
-    username: "postgres",
-    password: "noor2005",
-  );
-  await connection.open();
+class DatabaseHelper {
+  void queryData() async {
+    var connection = PostgreSQLConnection(
+      "localhost",
+      5432,
+      "clubs",
+      username: "postgres",
+      password: "noor2005",
+    );
+    await connection.open();
 
-  // ids = await connection.query("SELECT id FROM clubs");
-  // clubNames = await connection.query("SELECT clubname FROM clubs");
-  // categorys = await connection.query("SELECT category FROM clubs");
-  // meetingDays = await connection.query("SELECT meetingday FROM clubs");
-  // advisorNames = await connection.query("SELECT advisorname FROM clubs");
-  // advisorEmails = await connection.query("SELECT advisoremail FROM clubs");
-  monkey = await connection.query("SELECT * FROM public.monkey");
-  userdata = await connection.query("SELECT * FROM public.userdata");
-  // for (int i = 0; i < clubs.length; i++) {
-  //   categorySizes[clubs[i][2]] = (categorySizes[clubs[i][2]]! + 1);
-  // }
-for (int i = 0; i < userdata.length; i++) {
-                              if (userdata[i][2] == 'user1@bxscience.edu') {
-                              print('monk');
-                            }
-                            }
+    monkey = await connection.query("SELECT * FROM public.monkey");
+    userdata = await connection.query("SELECT * FROM public.userdata");
+    // for (int i = 0; i < clubs.length; i++) {
+    //   categorySizes[clubs[i][2]] = (categorySizes[clubs[i][2]]! + 1);
+    // }
+    for (int i = 0; i < userdata.length; i++) {
+      if (userdata[i][2] == 'user1@bxscience.edu') {
+        print('monk');
+      }
+    }
 
-  print(userdata[1][2]);
+    print(userdata[1][2]);
+  }
 }
+
+// void main() async {
+//   var connection = PostgreSQLConnection(
+//     "localhost",
+//     5432,
+//     "clubs",
+//     username: "postgres",
+//     password: "noor2005",
+//   );
+//   await connection.open();
+
+//   monkey = await connection.query("SELECT * FROM public.monkey");
+//   userdata = await connection.query("SELECT * FROM public.userdata");
+//   // for (int i = 0; i < clubs.length; i++) {
+//   //   categorySizes[clubs[i][2]] = (categorySizes[clubs[i][2]]! + 1);
+//   // }
+//   for (int i = 0; i < userdata.length; i++) {
+//     if (userdata[i][2] == 'user1@bxscience.edu') {
+//       print('monk');
+//     }
+//   }
+
+//   print(userdata[1][2]);
+// }
