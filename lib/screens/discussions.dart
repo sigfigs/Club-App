0752b0ce673ce1b4a320a7ceaf4a1c2852eb7208
bedirbxsci <bedir@bxscience.edu>
2023-02-01 19:58:20 -0500

    lol

--- lib/screens/discussions.dart
+++ lib/screens/discussions.dart
@@ -2,3 +2,26 @@ import 'package:flutter/material.dart';
 import 'clubhome.dart';
 import 'home.dart';
 import 'signin.dart';
+
+class Discussion extends StatefulWidget {
+  final String clubName;
+  final String clubDay;
+  final String clubAdvisor;
+  final String clubCategory;
+  const Discussion(
+      {super.key,
+      required this.clubName,
+      required this.clubDay,
+      required this.clubAdvisor,
+      required this.clubCategory});
+
+  @override
+  State<Discussion> createState() => _Discussion();
+}
+
+class _Discussion extends State<Discussion> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
